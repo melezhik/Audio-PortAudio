1da5b68471849d05dc7162f27f8b66f4eabe299e
Jonathan Stowe <jns+git@gellyfish.co.uk>
2016-03-15 11:38:21 +0000

    Carry on POD

--- lib/Audio/PortAudio.pm
+++ lib/Audio/PortAudio.pm
@@ -81,15 +81,24 @@ useful :) So most of the credit probably goes to him.
 The constructor doesn't currently take any arguments. It will cause C<initialize()>
 for you.
 
+=head2 method version
+
+    method version() returns Str
+
+This returns the portaudio library version string that may be useful for diagnostic
+purposes.
+
 =head2 method initialize
 
-    method initialize() returns Int
+    method initialize() returns Bool
 
 This starts the portaudio service and will initialise all of the host API drivers
 found, which may (depending on the configuration,) cause the drivers to emit some
 output (typically ALSA and JACK will do this.) You probably don't need to call this
 yourself as it is called by the constructor, though may be necessary after 
-C<terminate> if you didn't actually end your program.
+C<terminate> if you didn't actually end your program. If there was a problem
+initializing the library then an exception will be thrown.
+
 
 =head2 method terminate
 
@@ -127,21 +136,143 @@ device.
 
     method host-api-index(HostApiTypeId $type) returns Int
 
+This returns the index number of the host API as used in the
+C<Audio::PortAudio::DeviceInfo> given the host API type, and
+can be used to enumerate devices of a certain type from the
+C<devices> list. C<HostApiTypeId> is an enumeration with the
+following values:
+
+=item InDevelopment
+
+=item DirectSound
+
+=item MME
+
+=item ASIO
+
+=item SoundManager
+
+=item CoreAudio
+
+=item OSS
+
+=item ALSA
+
+=item AL
+
+=item BeOS
+
+=item WDMKS
+
+=item JACK
+
+=item WASAPI
+
+=item AudioScienceHPI
+
+It is unlikely that more than a couple of these will actually
+be available on any given system.
+
 =head2 method host-api
 
     method host-api(HostApiTypeId $type) returns HostApiInfo
+
+Given a C<HostApiTypeId> as described above this will return
+a L<HostApiInfo> object  that describes the host api on this
+system.  
 
 =head2 method default-output-device
 
     method default-output-device() returns DeviceInfo
 
+This returns a L<Audio::PortAudio::DeviceInfo> object that
+describes the device that will be used for output if the
+default output stream is asked for. Depending on the
+configuration of your system this may differ from the
+C<default-input-device>.  An exception will be thrown
+if there was a problem determining the device.
+
 =head2 method default-input-device
 
     method default-input-device() returns DeviceInfo
 
+This returns a L<Audio::PortAudio::DeviceInfo> object that
+describes the device that be used for input if the
+default input stream is asked for.  Depending on the
+configuration of your system this may differ from the
+C<default-outpur-device>.  An exception will be thrown
+if there was a problem determining the device.
+
 =head2 method open-default-stream
 
     method open-default-stream(Int $input = 0, Int $output = 2, StreamFormat $format = StreamFormat::Float32, Int $sample-rate = 44100, Int $frames-per-buffer = 256) returns Stream
+
+This opens a stream for reading and/or writing on the default device,
+returning a L<Audio::PortAudio::Stream> object or throwing an
+exception if there was a problem opening the stream.
+
+The default values will almost certainly B<not> work for a lot of
+applications, of particular importance is C<format> which should
+be a member of the enumeration L<Audio::PortAudio::StreamFormat>:
+
+=item Float32
+
+=item Int32 
+
+=item Int24
+
+=item Int16
+
+=item Int8
+
+=item UInt8
+
+=item CustomFormat
+
+=item NonInterleaved
+
+Which should firstly match the capabilities of your device and
+also C<must> match the bit-size and type of the data that is
+written, if this is not adhered to then you are likely to see
+segfaults or other memory violation errors.  The C<CustomFormat>
+is unlikely to be used unless you have created your own
+portaudio backend.  The C<NonInterleaved> value can be ORed
+(C<+|>) with another value to tell the stream to expect the
+data as separate arrays of data for each channel rather than
+"interleaved" (samples for each channel in a "frame" appear
+consecutively in the data,) this may be more convenient for some
+applications especially with a high channel count.
+
+The C<$buffer-size> may also be critical for some backends (such as
+"JACK",) that require this to match the value configured in the
+backend and that all reads and writes are of the same size, you
+will need to consult the configuration of the backend to determine
+what this value should be.  Some backends (such as ALSA,) on the other hand,
+don't seem to be quite as sensitive and this can be set to some
+value that your application can easily handle in the time available (
+that is (buffer-size * channels)/samplerate seconds.)
+
+The C<$samplerate> should match the capabilities or configuration of your
+device and the samplerate of the data that is being written, if you
+pass data to C<write> which does not match this it is unlikely to 
+playback correctly (you may be able to use L<Audio::Convert::Samplerate|https://github.com/jonathanstowe/Audio-Convert-Samplerate>
+to adjust this in your application.)
+
+The C<$input> and C<$output> parameters indicate the number of channels
+to be opened for input and output respectively, 0 indicating that the
+stream will not be opened for either reading or writing.  The value
+should match the number of channels present in the device (or a smaller
+value,) some devices may indicate a larger number of channels than are
+actually used or connected, if a smaller number of channels is requested
+than the device presents then they will be taken in the order that the
+backend supports them, there is no way of specifying a specific range of
+channels, though you could use the NonInterleaved option to format and
+ignore the channels you aren't interested in.  The implication for
+backends that are explicitly designed for multi channel applications
+(such as JACK) is that you may not be able to distinguish named 
+individual channels within the device. Sorry about that, but it appears
+to be a portaudio limitation.
+
 
 =head2 method open-stream
 
@@ -406,6 +537,12 @@ class Audio::PortAudio {
 
     submethod BUILD() {
         self.initialize();
+    }
+
+    sub Pa_GetVersionText() returns Str is native('portaudio', v2) { * };
+
+    method version() returns Str {
+        Pa_GetVersionText();
     }
     
     sub Pa_Initialize() returns int32 is native('portaudio',v2) {...}
