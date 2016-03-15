1e26efe256c2f721f62eb70b5376e0a2ccda7084
Jonathan Stowe <jns+git@gellyfish.co.uk>
2016-03-15 18:24:04 +0000

    First round of POD

--- lib/Audio/PortAudio.pm
+++ lib/Audio/PortAudio.pm
@@ -231,60 +231,344 @@ be a member of the enumeration L<Audio::PortAudio::StreamFormat>:
 
 =item NonInterleaved
 
-Which should firstly match the capabilities of your device and
-also C<must> match the bit-size and type of the data that is
-written, if this is not adhered to then you are likely to see
-segfaults or other memory violation errors.  The C<CustomFormat>
-is unlikely to be used unless you have created your own
-portaudio backend.  The C<NonInterleaved> value can be ORed
-(C<+|>) with another value to tell the stream to expect the
-data as separate arrays of data for each channel rather than
-"interleaved" (samples for each channel in a "frame" appear
-consecutively in the data,) this may be more convenient for some
-applications especially with a high channel count.
+Which should firstly match the capabilities of your device and also
+C<must> match the bit-size and type of the data that is written, if this
+is not adhered to then you are likely to see segfaults or other memory
+violation errors.  The C<CustomFormat> is unlikely to be used unless you
+have created your own portaudio backend.  The C<NonInterleaved> value
+can be ORed (C<+|>) with another value to tell the stream to expect the
+data as separate arrays of data for each channel rather than "interleaved"
+(samples for each channel in a "frame" appear consecutively in the data,)
+this may be more convenient for some applications especially with a high
+channel count.
 
 The C<$buffer-size> may also be critical for some backends (such as
-"JACK",) that require this to match the value configured in the
-backend and that all reads and writes are of the same size, you
-will need to consult the configuration of the backend to determine
-what this value should be.  Some backends (such as ALSA,) on the other hand,
-don't seem to be quite as sensitive and this can be set to some
-value that your application can easily handle in the time available (
-that is (buffer-size * channels)/samplerate seconds.)
+"JACK",) that require this to match the value configured in the backend
+and that all reads and writes are of the same size, you will need
+to consult the configuration of the backend to determine what this
+value should be.  Some backends (such as ALSA,) on the other hand,
+don't seem to be quite as sensitive and this can be set to some value
+that your application can easily handle in the time available ( that is
+(buffer-size * channels)/samplerate seconds.)
 
-The C<$samplerate> should match the capabilities or configuration of your
-device and the samplerate of the data that is being written, if you
-pass data to C<write> which does not match this it is unlikely to 
-playback correctly (you may be able to use L<Audio::Convert::Samplerate|https://github.com/jonathanstowe/Audio-Convert-Samplerate>
+The C<$samplerate> should match the capabilities or configuration
+of your device and the samplerate of the data that is being
+written, if you pass data to C<write> which does not match
+this it is unlikely to playback correctly (you may be able to use
+L<Audio::Convert::Samplerate|https://github.com/jonathanstowe/Audio-Convert-Samplerate>
 to adjust this in your application.)
 
 The C<$input> and C<$output> parameters indicate the number of channels
 to be opened for input and output respectively, 0 indicating that the
-stream will not be opened for either reading or writing.  The value
-should match the number of channels present in the device (or a smaller
-value,) some devices may indicate a larger number of channels than are
-actually used or connected, if a smaller number of channels is requested
-than the device presents then they will be taken in the order that the
-backend supports them, there is no way of specifying a specific range of
-channels, though you could use the NonInterleaved option to format and
-ignore the channels you aren't interested in.  The implication for
-backends that are explicitly designed for multi channel applications
-(such as JACK) is that you may not be able to distinguish named 
-individual channels within the device. Sorry about that, but it appears
-to be a portaudio limitation.
+stream will not be opened for either reading or writing.  The value should
+match the number of channels present in the device (or a smaller value,)
+some devices may indicate a larger number of channels than are actually
+used or connected, if a smaller number of channels is requested than the
+device presents then they will be taken in the order that the backend
+supports them, there is no way of specifying a specific range of channels,
+though you could use the NonInterleaved option to format and ignore the
+channels you aren't interested in.  The implication for backends that
+are explicitly designed for multi channel applications (such as JACK) is
+that you may not be able to distinguish named individual channels within
+the device. Sorry about that, but it appears to be a portaudio limitation.
 
 
 =head2 method open-stream
 
     method open-stream(StreamParameters $in-params, StreamParameters $out-params, Int $sample-rate = 44100, Int $frames-per-buffer = 256) returns Stream
 
+This returns the L<Audio::PortAudio::Stream> opened with the parameters supplied
+in the L<Audio::PortAudio::StreamParameters> C<$in-params> and C<$out-params>, described
+below, if either input or output is not required then a StreamParameters type object
+can be passed for either parameter.  C<$sample-rate> and C<$frames-per-buffer> is the
+same as for C<open-default-stream> and apply to both input and output.
+
+This will throw an exception if the stream cannot be opened with the parameters supplied,
+you can validate the parameters prior to making this call with C<is-format-supported>
+described below.
+
 =head2 method is-format-supported
 
     method is-format-supported(StreamParameters $input, StreamParameters $output, Int $sample-rate) returns Bool
 
+This returns a Boolean to indicate whether the L<StreamParameters> C<$input> and C<$output>
+and the sample rate would work for C<open-stream>, if it returns False it is likely that
+C<open-stream> would throw an exception with the same parameters.
+
 =head2 method error-text
 
     method error-text(Int $error-code) returns Str
+
+This returns the appropriate error text for the integer code returned by the  pulseaudio
+API calls, a negative number indicating that it is an error response.  As most of the
+methods that call the portaudio API will infact throw an exception with the appropriate
+message this may not be all that useful.
+
+=head1 Audio::PortAudio::DeviceInfo
+
+This is the class that describes the portaudio devices as discoverd by C<devices> or
+C<device-info>.  It has the following members:
+
+
+=head2 struct-version
+
+This is the version of this API structure used, it probably isn't all that useful
+unless you somehow have mixed versions of backend and portaudio and things aren't
+working correctly.
+
+=head2 name;
+
+This is the name of the device that will commonly be displayed in a user interface.
+
+=head2 api-version;
+
+This is the C<host-api-index> (not the host api type,) of the backend API that
+supports the device.  See the C<host-api> method to get details of the actual
+host API being used.
+
+=head2 max-input-channels;
+
+This is the maximum number of input channels that the device reports, it should be
+noted that not all of the number may be usable or connected to a physical device
+as some drivers may, for instance, examine the underlying hardware to get this
+value and some of the potential inputs may not be physically connected (laptops
+with generic audio chips are an example of this,) however it is probably okay
+to request up to this number of channels, they just may not work.
+
+=head2 max-output-channels
+
+The maximum output channels that the device reports.  The same caveats apply as
+for C<max-input-channels>.
+
+=head2 default-low-input-latency
+
+This is a suggested value for input latency for low latency applications, expressed
+as a Num fraction of a second. This can be used in a C<StreamParameters>.  Some
+backends such as "JACK" may report this (and the other latency values below,) as
+being 0 for "client" applications, in which case it might be necessary to use the
+appropriate value for the "default device" of the host API.
+
+=head2 default-low-output-latency
+
+This is a suggested value for output latency for low latency applications, expressed
+as a Num fraction of a second. This can be used in a C<StreamParameters>.
+
+=head2 default-high-input-latency;
+
+This is a suggested value for input latency for high latency applications, expressed
+as a Num fraction of a second.  This is possibly a better value for Perl applications
+which are likely to be "high latency".
+
+=head2 default-high-output-latency
+
+This is a suggested value for output latency for high latency applications, expressed
+as a Num fraction of a second.  This is possibly a better value for Perl applications
+which are likely to be "high latency".
+
+=head2 default-sample-rate;
+
+This is the default sample rate for the device and will probably be the one that you
+want to use when you are opening a stream on the device unless you definitely know
+that the device can adjust itself appropriately.  The usual caveats about using the
+correct sample rate for input and output data apply. Portaudio expresses this as a
+floating point number ( a Num,) but will almost without exception be an Int.  The
+C<open-stream> and C<open-default-stream> take C<sample-rate> as an Int which will
+be coerced appropriately.
+
+=head2 method host-api
+
+    method host-api() returns HostApiInfo
+
+This returns the L<Audio::PortAudio::HostApiInfo> object representing the host api
+of this device, which contains useful information about the backed API. 
+
+=head1 Audio::PortAudio::HostApiInfo
+
+This class represents the details of a host API or "backend" as returned by several
+methods described above.
+
+
+=head2 struct-version
+
+This is a version number that is internal to the portaudio implementation.
+
+=head2 type
+
+This is the HostApiTypeId and the value will be one of the enumeration described
+above.
+
+
+=head2 name
+
+This is the name of the host API, such as "ALSA", "JACK" etc.
+
+=head2 device-count
+
+This is the number of devices that are recorded for this host API and could
+be used to enumerate the devices.
+
+=head2 default-input-device
+
+This is the "device index" of the default input device for this backend, this
+can be used to get, for example, the default latency for a backend such as
+JACK that might not report the correct value for its clients, by passing this
+value to the C<device-info> method of the L<Audio::PortAudio> object to get
+the appropriate L<Audio::PortAudio::DeviceInfo> object.
+
+=head2 default-output-device 
+
+This is the "device index" of the default output device for this backend, this
+can be used to get, for example, the default latency for a backend such as
+JACK that might not report the correct value for its clients, by passing this
+value to the C<device-info> method of the L<Audio::PortAudio> object to get
+the appropriate L<Audio::PortAudio::DeviceInfo> object. 
+
+This will usually the same as the input device but this shouldn't be relied on.
+
+=head1 Audio::PortAudio::StreamParameters
+
+Objects of this class are passed as the C<$in-params> and C<$out-params> of
+the method C<open-stream> and describe the details required of the input
+or outputs to be created. All of the attributes must be filled in unless
+otherwise noted. The validity or otherwise of a particular set of parameters
+can be checked with C<> before actually using them to open a stream.
+
+=head2 device
+
+This is the "device index" of the required device (that is 0 .. device-count -1)
+The device should support the other parameters provided.
+
+=head2 channel-count
+
+This is the integer number of channels that should be opened on the stream for
+the input or output, it should be equal to or less than the maximum number of
+channels supported by the device.
+
+=head2 sample-format
+
+This is the integer value of an item of the enumeration C<SampleFormat>
+described above, it should agree with the type of the data that
+you are intending to read from or write to the stream (otherwise
+unexpected results may ensue,) not all devices or backend APIs will
+support all format types and you may have to consult the documentation
+or configuration.  The most commonly supported types are C<Float32>
+and C<Int16>.
+
+=head2 suggested-latency
+
+This is the suggested latency for this stream as a Num
+fraction of a second.  You probably want to used the device's
+<default-high-input-latency> or <default-high-output-latency> though
+some backends may entirely ignore this if they have a fixed latency value.
+
+=head2 host-api-specific-streaminfo
+
+This is a Pointer to some data that may be supplied to the backend, it is B<not> 
+required by portaudio and should generally be left uninitialised.
+
+=head1 Audio::PortAudio::Stream
+
+Objects of this type are returned by C<open-default-stream> and C<open-stream>
+and are the primary interface for reading and writing data from/to a device
+and for getting the status of the audio stream.
+
+=head2 method start
+
+    method start() returns Bool
+
+This starts the stream, making it available for reading or writing. Attempting
+to read or write without first calling start will give rise to an exception.
+
+An exception will be thrown if the stream cannot be started.
+
+=head2 method close
+
+    method close() returns Bool
+
+This closes the stream, releasing the device.  If any reads or writes are
+attempted after it has been closed then an exception will be thrown.
+You should always close a stream after you are done with it as the API
+will continue attempting to populate the underlying buffers until closed.
+
+If you attempt to close any already closed stream an exception will be
+thrown.
+
+=head2 method stopped
+
+    method stopped() returns Bool
+
+This returns a Bool to indicate whether the stream is running, it will
+return True before C<start> has been called and after C<close>.
+
+=head2 method active
+
+    method active() returns Bool
+
+This returns a Bool to indicate whether the stream is running, it will
+return True after C<start> has been called and before C<close>.
+
+=head2 method write-available
+
+        method write-available() returns Int
+
+This returns the number of frames that can be written to the stream
+without the C<write> call blocking, It may throw an exception if the
+stream isn't started or there was some other problem with the stream.
+
+=head2 method write
+
+        method write(CArray $buf, Int $frames) returns Int
+
+This writes the data in the provided CArray, which must contain data
+of the correct type as per the C<StreamFormat> (e.g. num32, int16 etc,)
+and should contain C<$frames> * C<channels> elements.
+
+The data must be written with a frequency such that the underlying buffer
+is kept filled, an exception will be thrown if the data isn't being
+fed fast enough and a "buffer under run" occurs before the next write.
+
+For some backends which require a fixed buffer size the number of frames
+provided should match the buffer size provided when opening the stream,
+not doing so may result in less than ideal results.
+
+If the stream isn't started or there is some other problem writing the
+data then an exception will be thrown.
+
+It is deliberate that there is no sugared multi candidates for providing
+higher level Perl arrays with the data, as the overhead in copying the
+data will typically eat up too much time to keep the buffer filled.
+Hopefully this will change in the future.
+
+=head2 method read-available
+
+        method read-available() returns Int
+
+This returns the number of frames available to be read from a stream, it
+may return 0 if the stream isn't opened for input.  It may throw an
+exception if there was some problem with the stream.
+
+=head2 method read
+
+        method read(Int $frames, Int $num-channels, Mu:U $type) returns CArray
+
+This reads the specified number of frames of C<$num-channels> from a stream that
+is opened for input, the number of channels should agree with that provided when
+opening the stream, and the C<$type> should correspond to the type of the data
+that will be used for the C<StreamFormat> (i.e. num32, int16, etc,).  The CArray
+returned will be typed accordingly and will contain C<$frames> * C<$num-channels>
+elements.
+
+If you do not read from the stream fast enough you may experience buffer over-runs,
+choppy input, or other artefacts, some backends may consider it fatal and an
+exception may be raised or the stream closed.
+
+Usually you should be prepared to read the number of frames that were specified
+when the stream was opened, if the backend requires a specific number of frames
+for the buffer then this should always be used when reading from the stream.
+
+If there is a problem reading, or the stream has been closed then an exception
+will be thrown.
+
 
 
 =end pod
